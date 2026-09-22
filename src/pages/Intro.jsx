2659e57f04ef842375fb450d09243b5deee918bf
hihyena37 <hihyena37@gmail.com>
2026-09-22 17:45:04 +0900

    인트로 제작중...

--- src/pages/Intro.jsx
+++ src/pages/Intro.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+
 import IntroMenu from '../components/IntroMenu'
+import introBg from '../assets/intro-bg.gif'
+
+import './Intro.css'
 
 const Intro = ({ setPage, setCurrentPage }) => {
     return (
-        <main>
+        <main
+            className="intro"
+            style={{
+                backgroundImage: `url(${introBg})`
+            }}
+        >
             <IntroMenu
                 setPage={setPage}
                 setCurrentPage={setCurrentPage}
